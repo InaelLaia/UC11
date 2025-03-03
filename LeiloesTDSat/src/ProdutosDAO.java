b21fbf9a055282ba8448f96cc09e41fa5bf0aeef
InaelLaia <inaelw@gmail.com>
2025-03-03 19:21:24 -0300

    listagem de produtos funcionando

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -46,7 +46,29 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        conn = new conectaDAO().connectDB();
+        try {
+            String sql = "SELECT * FROM produtos";  
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();  
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));  
+                produto.setNome(resultset.getString("nome"));  
+                produto.setValor(resultset.getInt("valor"));  
+                produto.setStatus(resultset.getString("status"));  
+
+                listagem.add(produto);  
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro ao listar produtos: " + ex.getMessage());
+        } try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao desconectar: " + ex.getMessage());
+        }
         return listagem;
     }
     
